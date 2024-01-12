64c826471fdb0f219f7bc6a64a28a662c0eeaacb
BimaFdilana <Bima.fadilana01@gmail.com>
2024-01-13 02:25:54 +0700

    new-1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages/blank-page', ['type_menu' => '']);
+    return view('pages.blank-page');
 });
+
+Route::get('/register', function () {
+    return view('pages.auth.auth-register');
+})->name('register');
+
